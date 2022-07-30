6e713eea245d16b82e6e7314c1f5f5bb7e4babbb
RodolpheBeloncle <rbeloncle@gmail.com>
2022-07-30 12:30:31 +0200

    add content type

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -4,10 +4,16 @@ const BASE_URL =  'https://api.rodolphebeloncle.com/api/';
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json'
+  },
   withCredentials: true,
 });
 
 export const userRequest = axios.create({
   baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json'
+  },
   withCredentials: true,
 });
