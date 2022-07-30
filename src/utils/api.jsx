4169eb22f74a15bae51c75e5ecf66c520ac283c4
RodolpheBeloncle <rbeloncle@gmail.com>
2022-07-30 11:55:37 +0200

    fix url

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL =  'http://api.rodolphebeloncle.com/api/';
+const BASE_URL =  'https://api.rodolphebeloncle.com/api/';
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
