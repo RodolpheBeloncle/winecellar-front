e7155e0512daecc29a94a4a88e73709c4901faa8
RodolpheBeloncle <rbeloncle@gmail.com>
2022-07-30 15:15:07 +0200

    remove cors

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -5,11 +5,9 @@ const BASE_URL = 'https://api.rodolphebeloncle.com/api/';
 export const publicRequest = axios.create({
   withCredentials: true,
   baseURL: BASE_URL,
-  headers: {'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'}
 });
 
 export const userRequest = axios.create({
   withCredentials: true,
   baseURL: BASE_URL,
-  headers: {'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'}
 });
