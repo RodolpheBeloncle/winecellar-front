82973ce578f81968dcd8e4d066a0b32c5485cca9
RodolpheBeloncle <rbeloncle@gmail.com>
2022-07-30 10:52:04 +0200

    change url adress for production

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL =  'http://localhost:8000/api/';
+const BASE_URL =  'https://api.rodolphebeloncle.com/api/';
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
