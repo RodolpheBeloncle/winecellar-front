e8f5cc8baca9d382255fcc329437131a26f660be
RodolpheBeloncle <rbeloncle@gmail.com>
2022-07-30 09:00:10 +0200

    test axios

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_API_URL;
+const BASE_URL =  'http://localhost:8000/api/';
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
-  withCredentials: false,
+  withCredentials: true,
 });
 
 export const userRequest = axios.create({
