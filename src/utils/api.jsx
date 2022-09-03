a346a54980035a1f040f013e824eb860b8a0050a
RodolpheBeloncle <rbeloncle@gmail.com>
2022-09-03 08:29:00 +0200

    deployment mode

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const BASE_URL = 'https://api.rodolphebeloncle.com/api/';
-const BASE_URL = 'http://localhost:8000/api/';
+const BASE_URL = 'https://api.rodolphebeloncle.com/api/';
+// const BASE_URL = 'http://localhost:8000/api/';
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
